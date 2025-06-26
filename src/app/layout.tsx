1af7873e0e847ab52cdb58e10261d48cf723a264
Antonis Manaras <antonis.manaras@gmail.com>
2025-06-26 16:41:28 +0000

    bring the containers back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       </head>
       <body className={cn('font-body antialiased flex flex-col min-h-screen')}>
         <AppHeader />
-        <main className="flex-grow px-4 sm:px-6 lg:px-8 py-8">
+        <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
           {children}
         </main>
         <AppFooter />
