b24e1cfff60e024890f605830ccb3b1d7e5b36cb
Antonios Manaras <antonis.manaras@gmail.com>
2025-07-28 20:20:23 +0100

    Initial commit

---
+++ src/lib/variables.ts
@@ -0,0 +1,44 @@
+// Set any item to undefined to remove it from the site or to use the default value
+
+export const GLOBAL = {
+  // Site metadata
+  username: "0x42SEC",
+  rootUrl: "https://zaggonaut.dev",
+  shortDescription: "Cyber Security Musings",
+  longDescription: "",
+  
+  // Social media links
+  // githubProfile: "https://github.com/RATIU5/zaggonaut",
+  // twitterProfile: "https://twitter.com/",
+  // linkedinProfile: "https://www.linkedin.com/",
+  
+  // Common text names used throughout the site
+  articlesName: "CyberThoughts",
+  projectsName: "Projects",
+  viewAll: "View All",
+  
+  // Common descriptions used throughout the site
+  noArticles: "No featured articles yet.",
+  noProjects: "No featured projects yet.",
+
+  // Blog metadata
+  blogTitle: "My Thoughts & Takes",
+  blogShortDescription: "Practical wisdom, unfiltered thoughts, and hot takes.",
+  blogLongDescription: "Web development, tech trends, and the occasional programming mishap.",
+
+  // Project metadata
+  projectTitle: "Projects and Code",
+  projectShortDescription: "A list of my web development projects and developer tools.",
+  projectLongDescription: "All of my projects, including both frontend and full-stack applications.",
+
+  // Profile image
+  profileImage: "me_round.png",
+
+  // Menu items
+  menu: {
+    home: "/",
+    // projects: "/projects",
+    blog: "/blog",
+    about: "/about"
+  }
+};
